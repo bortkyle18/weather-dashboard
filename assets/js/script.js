d61918e6ddcdc14a7d44304f09c5732d47e0016d
bortkyle18 <bortkyle18@gmail.com>
2022-05-14 22:38:26 -0400

    print list, clear list and print new with new city

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,31 +2,11 @@ var clientID = "Sw9ADFQMdRTFnIeMSqyPcg";
 var APIkey = "67mqRNvaaHn5QdfWrCTna5d97oAiv8E-L2rhNIQ0CfS9TIqsqV-dmw9UJJ6KmHlWW_Od--P0iRNlN3ujxHB0ILtCZGR-sKAUtkGgRXIf7MZqihLIq8E3_9kluVJ8YnYx";
 
 
-// var getEventRepo = function() {
-
-//     // Get entered city
-//     var cityEnter = $("#city").val();
-//     // pull up to 200 responses from the requested city - sorted by date of event
-//     var apiUrl = "https://app.ticketmaster.com/discovery/v2/events.json?size=200&city=" + cityEnter + "&sort=date,asc&apikey=syoZ02A5zn7KOdclO7ZeAyKBCV7U8DK7";
-
-//     $.ajax({
-//         type:"GET",
-//         url: apiUrl,
-//         async:true,
-//         dataType: "json",
-//         success: function(json) {
-//             eventsFound(json._embedded.events);
-//             // Parse the response.
-//             // Do other things.
-//         },
-//         error: function(xhr, status, err) {
-//             // This time, we do not end up here!
-//         }
-
-//     });
-// };
-
+// Find events and print to page with links
 var eventLi = function() {
+    // clear list
+    $("ul").empty()
+    // Create HTML for events to populate list
     var eventsFound = function(events) {
         for(var i = 0; i < events.length; i++) {
             var eventList = $("<li>")
@@ -67,13 +47,20 @@ var eventLi = function() {
     };
     getEventRepo();
 };
+// End of events
+
+
+// Find food and print to page with links
+var getYelpAPI = function() {
+    fetch("https://api.yelp.com/v3/businesses/search");
+};
+
+
 
 $("#button").on("click",eventLi);
 
 
-var getYelpAPI = function() {
-    fetch("https://api.yelp.com/v3/businesses/search");
-};
+
 
 
 
